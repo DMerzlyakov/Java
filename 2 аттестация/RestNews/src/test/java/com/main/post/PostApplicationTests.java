e1540d5ee69d964bcecf7256aeca0b9f72a88536
d.merzlyakov <65449169+DMerzlyakov@users.noreply.github.com>
2021-03-28 23:34:59 +0300

    Самостоятельная работа

---
+++ 2 аттестация/RestNews/src/test/java/com/main/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
